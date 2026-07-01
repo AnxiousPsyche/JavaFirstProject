2f560881728d50110777c74edccd00e98063f866
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-07-01 16:21:37 +0800

    Initial Java project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main { public static void main(String[] args) {
+    String name = "Bro Code";
+    int gta = 6;
+    double pi = 3.14159;
+    char gender = 'M';
+    boolean isAdmin = true;
+
+    System.out.println("Your name is " + name + " ,your gender is " + gender + ", and you're waiting for gta " + gta + " ,while solving " + pi);
+
+
+    }
+}
