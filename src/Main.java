02b5d61f6b0e1d7ed4118da3a264208b41311264
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-07-01 22:48:43 +0800

    mad libs lesson

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,34 @@ import java.util.Scanner;
 
 public class Main { public static void main(String[] args) {
 
-        double width = 0;
-        double height = 0;
-        double area = 0;
-    Scanner input = new Scanner(System.in);
+     Scanner input = new Scanner(System.in);
 
-    System.out.print("Enter the width: ");
-    width = input.nextDouble();
+     String adjective1;
+     String noun1;
+     String adjective2;
+     String verb1;
+     String adjective3;
 
-    System.out.print("Enter the height: ");
-    height = input.nextDouble();
+     System.out.print("Enter an adjective (description): ");
+     adjective1 = input.nextLine();
+    System.out.print("Enter a noun (animal or person): ");
+    noun1 = input.nextLine();
+    System.out.print("Enter an adjective (description): ");
+    adjective2 = input.nextLine();
+    System.out.print("Enter a verb ending in -ing (action): ");
+    verb1 = input.nextLine();
+    System.out.print("Enter an adjective (description): ");
+    adjective3 = input.nextLine();
 
-    area = width * height;
-
-    System.out.println("The area is: " + area + "cm²");
 
 
-    input.close();
+     System.out.println("Today I went to a" + adjective1 + "zoo.");
+    System.out.println("In an exhibit, I saw a " + noun1 + ".");
+    System.out.println(noun1 + "was" + adjective2 + "and" + verb1 + "!");
+    System.out.println("I was " + adjective3 + "!");
 
+
+
+     input.close();
     }
 }
