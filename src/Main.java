655f3020fbb63d1a074ab5164d9d1c36a012ff54
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-07-02 18:09:23 +0800

    if statements part 2

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,30 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
+        String name;
         int age;
+        boolean isStudent;
+
+        System.out.print("Enter your name: ");
+        name = input.nextLine();
 
         System.out.print("Enter your age: ");
         age = input.nextInt();
+
+        System.out.print("Are you a student(true/false): ");
+        isStudent = input.nextBoolean();
+
+        //Group 1
+        if (name.isEmpty()){
+            System.out.println("Who are you? why didn't you enter your name?");
+        }
+        else {
+            System.out.println("Hello " + name + "!");
+        }
+
+
+
+        // Group 2
 
         if (age >= 65){
             System.out.println("You are a senior!");
@@ -25,6 +45,15 @@ public class Main {
 
         else {
             System.out.println("You are a child");
+        }
+
+        //Group 3
+
+        if (isStudent) {
+            System.out.println("You are enrolled in my class😡");
+        }
+        else {
+            System.out.println("You are not enrolled in my class!👌");
         }
 
 
