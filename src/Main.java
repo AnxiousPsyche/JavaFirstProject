b80a9f782e5b105b2498b342f1182654193aa639
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-07-02 19:23:00 +0800

    math equation

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,25 @@
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        Random random = new Random ();
 
-        String name;
-        int age;
-        boolean isStudent;
+        int number1;
+        int number2;
+        int number3;
 
-        System.out.print("Enter your name: ");
-        name = input.nextLine();
-
-        System.out.print("Enter your age: ");
-        age = input.nextInt();
-
-        System.out.print("Are you a student(true/false): ");
-        isStudent = input.nextBoolean();
-
-        //Group 1
-        if (name.isEmpty()){
-            System.out.println("Who are you? why didn't you enter your name?");
-        }
-        else {
-            System.out.println("Hello " + name + "!");
-        }
+        number1 = random.nextInt(1, 7);
+        number2 = random.nextInt(1, 7);
+        number3 = random.nextInt(1, 7);
 
 
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
 
-        // Group 2
-
-        if (age >= 65){
-            System.out.println("You are a senior!");
-        }
-        else if (age >=18){
-            System.out.println("You are an a adult");
-        } else if (age == 0) {
-            System.out.println("You're just a baby!");
-        }
-        else if (age < 0) {
-            System.out.println("You haven't been born yet!");
-        }
-
-        else {
-            System.out.println("You are a child");
-        }
-
-        //Group 3
-
-        if (isStudent) {
-            System.out.println("You are enrolled in my class😡");
-        }
-        else {
-            System.out.println("You are not enrolled in my class!👌");
-        }
-
-
-        input.close();
+        ;
     }
 
 }
