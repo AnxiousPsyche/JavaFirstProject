eac19801fb85d32ddb7d059d09a37d7c6f1a193e
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-07-03 00:44:19 +0800

    printf statements

--- src/Main.java
+++ src/Main.java
@@ -9,28 +9,31 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        double radius;
-        double circumference;
-        double area;
-        double volume;
+        String name;
+        char firstletter;
+        int age;
+        double height;
+        boolean isEmployed;
 
+        System.out.print("What is your name: ");
+        name = input.nextLine();
+        System.out.print("What is/are your initial/s: ");
+        firstletter = input.next().charAt(0);
+        System.out.print("How old are you?: ");
+        age = input.nextInt();
+        System.out.print("How tall are you (meters): ");
+        height = input.nextDouble();
+        System.out.print("Are you employed? (true/false): ");
+        isEmployed = input.nextBoolean();
 
-        System.out.print("Enter the radius: ");
-        radius = input.nextDouble();
+        System.out.printf("Hello %s%n Your initial is %c%n You are %d years old %n Your height is %.2f", name, firstletter, age, height);
 
-        circumference = 2 * Math.PI * radius;
-        area = Math.PI * Math.pow(radius, 2);
-        volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+        if (isEmployed){
+            System.out.println(" and You're currently employed");
+        }
+        else {
+            System.out.println(" and You're currently unemployed");
+        }
 
-        System.out.printf("The circumference is: %.2fcm\n", circumference);
-        System.out.printf("The area is: %.2fcm\n", area);
-        System.out.printf("The volume is %.2fcm³\n", volume);
-
-
-        
-
-
-
-        input.close();
     }
 }
