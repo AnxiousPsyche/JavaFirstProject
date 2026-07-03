971e059f674e1d58246f6b0cce5f91c1be4794aa
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-07-03 14:07:11 +0800

    Compound interest calculator

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args){
 
-        //circumference = 2 * Math.PI * radius;
-        // area = Math.PI * Math.pow(radius, 2)
-        // volume = (4.0/3.0) * Math/PI * Math.pow(radius, 3)
-
         Scanner input = new Scanner(System.in);
 
-        String name;
-        char firstletter;
-        int age;
-        double height;
-        boolean isEmployed;
+        double principal;
+        double rate;
+        int timesCompounded;
+        int years;
+        double amount;
 
-        System.out.print("What is your name: ");
-        name = input.nextLine();
-        System.out.print("What is/are your initial/s: ");
-        firstletter = input.next().charAt(0);
-        System.out.print("How old are you?: ");
-        age = input.nextInt();
-        System.out.print("How tall are you (meters): ");
-        height = input.nextDouble();
-        System.out.print("Are you employed? (true/false): ");
-        isEmployed = input.nextBoolean();
+        System.out.print("Enter the principal amount: ");
+        principal = input.nextDouble();
 
-        System.out.printf("Hello %s%n Your initial is %c%n You are %d years old %n Your height is %.2f", name, firstletter, age, height);
+        System.out.print("Enter the interest rate (in %): ");
+        rate = input.nextDouble() / 100;
 
-        if (isEmployed){
-            System.out.println(" and You're currently employed");
-        }
-        else {
-            System.out.println(" and You're currently unemployed");
-        }
+        System.out.print("Enter the # of times compound per year: ");
+        timesCompounded = input.nextInt();
+
+        System.out.print("Enter the # of years: ");
+        years = input.nextInt();
+
+        amount = principal * Math.pow(1 + rate / timesCompounded, timesCompounded * years);
+
+        System.out.print("The amount after " + years + "is: $" + amount);
+
+
+        input.close();
 
     }
 }
