3b46dd0e3610c8cf21026e46fb019a7080ae6c54
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-07-04 22:45:30 +0800

    Ternary operators

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,10 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        // Decalare variables
-        double weight ;
-        double newWeight;
-        int choice;
+        int hours = 8;
 
-        // Welcome message
-        System.out.println("Weight Convertion Program");
-        System.out.println("1: Convert lbs to kgs");
-        System.out.println("2: Convert kgs to lbs");
+        String timeofDay = (hours < 12) ? "A.M" : "P.M";
 
-        System.out.print("Choose an option: ");
-        choice = input.nextInt();
-
-        if (choice== 1){
-            System.out.print("Enter the weight in lbs: ");
-            weight = input.nextDouble();
-            newWeight = weight * 0.453592;
-            System.out.println("The new weight in kgs is: " + newWeight);
-        }
-        else {
-            System.out.print("Enter the weight in kgs: ");
-            weight = input.nextDouble();
-            newWeight = weight * 2.20462;
-            System.out.println("The new weight in lbs is: " + newWeight);
-        }
-
-
-        input.close();
+        System.out.println(timeofDay);
     }
 }
