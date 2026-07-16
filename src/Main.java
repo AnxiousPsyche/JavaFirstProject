2d187406e1e195b390c58cc6e106789ee9a79118
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-07-16 14:56:55 +0800

    while loop lesson

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void  main(String [] args) {
 
-        Scanner input = new Scanner(System.in);
+       Scanner input = new Scanner(System.in);
 
-        double num1;
-        double num2;
-        char operator;
-        double result = 0;
+       int age = 0;
 
-        System.out.print("Enter the first number: ");
-        num1 = input.nextDouble();
+        do{
+           if (age <=0){
+               System.out.println("Age must be greater than 0.");
+           } else if (age <=13){
+               System.out.println("You must be above 13 years old to enter");
+           }
+            System.out.print("Enter your age: ");
+            age = input.nextInt();
+       }while (age <=13);
 
-        System.out.print("Enter the second number: ");
-        num2 = input.nextDouble();
+       System.out.println("Welcome");
 
-        System.out.print("Enter an operator (+, -, *, /, ^): ");
-        operator = input.next().charAt(0);
 
-        switch (operator){
-            case '+' -> result = num1 + num2;
-            case '-' -> result = num1 - num2;
-            case '*' -> result = num1 * num2;
-            case '/' -> result = num1 / num2;
-            case '^' -> result = Math.pow(num1, num2);
-        }
-
-        System.out.println(result);
 
         input.close();
     }
