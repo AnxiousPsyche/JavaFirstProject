b2c2c0c4daab4129aa14427683589cbb69bc67d1
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-07-16 18:05:24 +0800

    do while loop lesson

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,14 @@ public class Main {
 
        Scanner input = new Scanner(System.in);
 
-       int age = 0;
+       int number = 0;
 
-        do{
-           if (age <=0){
-               System.out.println("Age must be greater than 0.");
-           } else if (age <=13){
-               System.out.println("You must be above 13 years old to enter");
-           }
-            System.out.print("Enter your age: ");
-            age = input.nextInt();
-       }while (age <=13);
+        do {
+            System.out.println("Enter a number between 1 -10: ");
+            number = input.nextInt();
+        }while (number <1 || number > 10);
 
-       System.out.println("Welcome");
+        System.out.println("You picked " + number);
 
 
 
