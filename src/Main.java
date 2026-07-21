1ebf05d1097189111d2ea519609787d235a1d345
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-07-21 16:50:49 +0800

    roll dice program p1

--- src/Main.java
+++ src/Main.java
@@ -2,86 +2,48 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
-    static Scanner input = new Scanner (System.in);
-
     public static void main(String[] args) {
 
+        // JAVA DICE ROLLER PROGRAM
 
-        // JAVA BANKING PROGRAM FOR BEGINNERS
 
-        // DECLARE VARIABLES
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
 
-        double balance = 0;
-        boolean isRunning = true;
-        int choice;
+        int numOfDice;
+        int total = 0;
 
-        // DISPLAY MENU
+        System.out.print("Enter the # of dice to roll: ");
+        numOfDice = input.nextInt();
 
-        while (isRunning){
-            System.out.println("***************");
-            System.out.println("BANKING PROGRAM");
-            System.out.println("***************");
-            System.out.println("1. Show Balance");
-            System.out.println("2. Deposit");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Exit");
-            System.out.println("***************");
+        if (numOfDice > 0){
 
-            System.out.print("Enter your choice (1-4): ");
-            choice = input.nextInt();
-
-            switch(choice){
-                case 1 -> showBalance(balance);
-                case 2 -> balance += deposit();
-                case 3 -> balance -= withdraw(balance);
-                case 4 -> isRunning = false;
-                default -> System.out.println("INVALID CHOICE");
+            for(int i = 0; i < numOfDice; i++){
+                int roll = random.nextInt(1, 7);
+                System.out.println("You rolled:" + roll);
+                total += roll;
             }
+            System.out.println("total: " + total);
+
         }
-        System.out.println("***************************");
-        System.out.println("Thank you! have a nice day!");
-        System.out.println("***************************");
+        else {
+            System.out.println("# of dice must be greater than 0");
+        }
+
+        // GET THE TOTAL
+
+        //DISPLAY ASCII OF DICE
+
         input.close();
-
-
     }
-    static void showBalance(double balance){
-        System.out.println("***************");
-        System.out.printf("$%.2f\n", balance);
-    }
-    static double deposit(){
-
-        double amount;
-        System.out.print("Enter an amount to be deposited: ");
-        amount = input.nextDouble();
-
-        if (amount < 0){
-            System.out.println("***************");
-            System.out.println("Amount can't be negative");
-            return 0;
-        }
-        else {
-            return amount;
-        }
-    }
-    static double withdraw(double balance){
-
-        double amount;
-        System.out.print("Enter amount to be withdrawn: ");
-        amount = input.nextDouble();
-
-        if(amount > balance){
-            System.out.println("Insufficient Funds");
-            return 0;
-        }
-        else if(amount < 0){
-            System.out.println("Amount can't be negative");
-            return 0;
-        }
-        else {
-            return amount;
-        }
-
+    static void printDie(int roll){
+        String dice1 = """
+                 -------
+                |       |
+                |   ●   |
+                |       |
+                 -------
+                """;
+        System.out.println(dice1);
     }
 }
